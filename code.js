9e32f7c321afbfcfb4a489844f9371dd0721aaf5
kristjanjansen <kristjan.jansen@gmail.com>
2020-05-30 20:08:13 +0200

    fixes

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 figma.showUI(__html__);
+figma.ui.hide();
 figma.ui.onmessage = ({ message, points }) => {
   if (message === "createCircles") {
-    //console.log(points);
     const nodes = [];
     points.forEach(([x, y]) => {
       const circle = figma.createEllipse();
       circle.x = x;
       circle.y = y;
-      circle.resize(50, 50);
+      circle.resize(200, 200);
       circle.fills = [{ type: "SOLID", color: { r: 1, g: 0, b: 0 } }];
+      circle.opacity = 0.25;
       figma.currentPage.appendChild(circle);
       nodes.push(circle);
     });
